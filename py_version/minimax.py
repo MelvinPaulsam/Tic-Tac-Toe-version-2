9b6e4193d21f338c8ad18bb3a625678b3befb821
Melvin Paulsam <49990182+MelvinPaulsam@users.noreply.github.com>
2021-01-22 12:33:42 +0530

    Update minimax.py

--- py_version/minimax.py
+++ py_version/minimax.py
@@ -8,10 +8,9 @@ from os import system
 """
 An implementation of Minimax AI Algorithm in Tic Tac Toe,
 using Python.
-This software is available under GPL license.
-Author: Clederson Cruz
-Year: 2017
-License: GNU GENERAL PUBLIC LICENSE (GPL)
+
+Author: Melvin Paulsam P
+
 """
 
 HUMAN = -1
